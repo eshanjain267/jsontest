2250c981959a0f1ff1f508e42442e929e86ee8e5
eshanjain267 <eshanjain267@gmail.com>
2019-02-12 14:33:45 +0530

    new commit adding gson update

--- app/src/main/java/step/first/jsontest/MainActivity.java
+++ app/src/main/java/step/first/jsontest/MainActivity.java
@@ -1,9 +1,12 @@
 package step.first.jsontest;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.AsyncTask;
+import android.os.Bundle;
 import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,10 +15,9 @@ import android.widget.Toast;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import com.google.gson.*;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
@@ -40,6 +42,7 @@ public class MainActivity extends AppCompatActivity {
         bt = (Button)findViewById(R.id.bt);
         tv1 =(TextView)findViewById(R.id.tV5);
 
+                final Gson js = new Gson();
 
         bt.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,22 +51,40 @@ public class MainActivity extends AppCompatActivity {
                 if((!et1.getText().toString().isEmpty()) ||(!et2.getText().toString().isEmpty()) || (!et3.getText().toString().isEmpty()))
                 {
                     try {
-                        JSONObject obj = new JSONObject();
-                        obj.put("name", et1.getText().toString().trim());
-                        obj.put("age",Integer.parseInt(et2.getText().toString().trim()));
-                        obj.put("sal",Integer.parseInt(et3.getText().toString().trim()));
 
-                        String json = obj.toString();
-                        mytask mt = new mytask();
-                        mt.execute(json);
+                        person p = new person();
+                        p.setName(et1.getText().toString().trim());
+                        p.setAge(Integer.parseInt(et2.getText().toString().trim()));
+                        p.setSal(Integer.parseInt(et3.getText().toString().trim()));
+
+                        String json = js.toJson(p, person.class);
+
+                        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+                        NetworkInfo ni = cm.getActiveNetworkInfo();
+                        if (ni != null && cm != null) {
 
 
+                            mytask mt = new mytask();
+                            mt.execute(json);
+
+
+                        }
+                        else
+                        {
+                            Toast.makeText(MainActivity.this, "Please check Your Connection", Toast.LENGTH_LONG).show();
+
+                        }
                     }
-                    catch(JSONException jex)
-                    {
-                        Toast.makeText(MainActivity.this,"error"+jex, Toast.LENGTH_LONG).show();
-                    }
+                    catch(Throwable ex)
+                        {
+                            Toast.makeText(MainActivity.this, "error" + ex, Toast.LENGTH_LONG).show();
+                        }
 
+
+                }
+                else
+                {
+                    Toast.makeText(MainActivity.this,"Please Enter Right Data", Toast.LENGTH_LONG).show();
 
                 }
 
@@ -90,7 +111,7 @@ public class MainActivity extends AppCompatActivity {
             String ans ="ok";
             try {
                 String jsonstr = strings[0];
-                URL url = new URL("http://192.168.1.214:2030/Mywhatsapp/jsontest.jsp");
+                URL url = new URL("http://192.168.1.214:2030/Mywhatsapp/gsontest.jsp");
                 HttpURLConnection con = (HttpURLConnection)url.openConnection();
                 con.setRequestMethod("POST");
                 con.setRequestProperty("Content-Type","application/json");
---
+++ app/src/main/java/step/first/jsontest/person.java
@@ -0,0 +1,33 @@
+package step.first.jsontest;
+
+public class person {
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getSal() {
+        return sal;
+    }
+
+    public void setSal(int sal) {
+        this.sal = sal;
+    }
+
+    private int sal;
+
+}
